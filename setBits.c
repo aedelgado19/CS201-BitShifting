a1ca30bfd90f5021dd2ab62eb71a3a35ac6a85af
aedelgado19 <aedelgado19@gmail.com>
2021-04-26 20:49:07 -0700

    fixed

--- setBits.c
+++ setBits.c
@@ -8,40 +8,14 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-
-int checkBit(int index, int num);
-void print(int bit);
-
-int main(int argc, char *argv[]){
-  unsigned short output = 0;
-  int bit = 0;
-  printf("TODO - check if not a number\n");
-  
-  //check if the user entered numbers
-  if(argc < 2){
-    printf("Error: You did not enter any numbers.\n");
-    exit(1);
-  }
-  
-  //check range
-  for(int j = 1; j < argc; ++j){
-    int num = atoi(argv[j]);
-    if(num < 0 || num > 15){ 
-      printf("Error: a number was out of range: %d\n", num);
-      exit(1);
-    }
-    output = output | (int) (1 << num); //shift num left 1
-  }
-
-  for(int k = 15; k >= 0; k--){
-    bit = checkBit(k, output);
-    print(bit);
-  }
-  return 0;
-}
+#include <ctype.h>
+#include <string.h>
+#define MAX 3
 
 /* A function to check which are switched on and off */
-int checkBit(int index, int num){
+int
+checkBit(int index, int num)
+{
   if((1 << index) & num){
     return 1;
   }
@@ -49,11 +23,60 @@ int checkBit(int index, int num){
 }
 
 /* A function to print the bits out */
-void print(int bit){
+int
+print(int bit)
+{
   if(bit == 0){
     printf("0");
+    return 0;
   }
   if(bit == 1){
     printf("1");
+    return 1;
   }
+  return -1;
+}
+
+int
+main(int argc, char *argv[])
+{
+  unsigned short output = 0;
+  int bit = 0;
+  int success = 0;
+  
+  //check if the user entered numbers
+  if(argc < 2){
+    printf("Error: You did not enter any numbers. Terminating program.\n");
+    exit(1);
+  }
+  
+  //check validity (range, is digit)
+  for(int j = 1; j < argc; ++j){
+    int num = atoi(argv[j]);
+
+    //check if the input is digit
+    for(int k = 0; k < strnlen(argv[j], MAX); k++){
+      if(!isdigit(argv[j][k])){
+	printf("Error: that was not a digit. Terminating program.\n");
+	exit(1);
+      }
+    }
+    
+    //range:
+    if(num < 0 || num > 15){ 
+      printf("Error: a number was out of range: %d. Terminating program.\n", num);
+      exit(1);
+    }
+    output = output | (int) (1 << num); //shift num left 1
+  }
+
+  for(int k = 15; k >= 0; k--){
+    bit = checkBit(k, output);
+    success = print(bit);
+    if(success == -1){
+      exit(1); //check return code for success
+    }
+  }
+  printf("\n");
+  return 0;
 }
