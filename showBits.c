4de96dfc5eacf1df14374619eea2b8afa92d233c
aedelgado19 <aedelgado19@gmail.com>
2021-04-26 21:34:21 -0700

    yay

--- showBits.c
+++ showBits.c
@@ -9,37 +9,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#define MAX 3
 
-int
-checkBits(char* arg, char* arg2){
-  
-  
+/* Function to check if the bits share 1's 
+   then print out the output */
+void
+checkBits(char* argA, char* argB){
+
+  //save as a long
+  int arg1 = (int) strtol(argA, NULL, 0);
+  int arg2 = (int) strtol(argB, NULL, 0);
+
+  //check which bits are both 1
+  for(int i = 0; i < 32; i++){
+    if((1 << i) & (arg1 & arg2)){
+      printf("%d, ", i);
+    }
+  }
+
+  //print out the final output
+  unsigned int answer = arg1 & arg2;
+  printf("in common. Hexadecimal: %X, Signed: %d, Unsigned: %u.\n", answer, answer, answer);
 }
 
 int
 main(int argc, char* argv[])
 {
-  int success = 0;
-  
   //make sure the user inputs the right amount of numbers
   if(argc != 3){
     printf("Error: please provide two hexadecimal numbers as arguments to the program.\n");
     printf("Terminating program.\n");
     exit(1);
   }
-
-  //check that the input is in hexadecimal
-  for(int i = 1; i < argc; i++){
-    for(int j = 0; j < strnlen(argv[j], MAX); j++){
-      if(!isxdigit(argv[i][j])){
-	printf("Error: that was not in hexadecimal. Terminating program.\n");
-	exit(1);
-      }
-    }
-  }
-  success = checkBits(argc[1], argc[2]);
+  checkBits(argv[1], argv[2]);
   
   return 0;
 }
